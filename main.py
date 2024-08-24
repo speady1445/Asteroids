bab1e79f3053898fef8a6d4c0546a5eb9b6f5a31
speady1445 <sykora1445@gmail.com>
2024-08-24 12:07:03 +0200

    Add player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main() -> None:
@@ -14,13 +15,17 @@ def main() -> None:
     clock = pygame.time.Clock()
     delta_time = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
         delta_time = clock.tick(60) / 1000
 
 
